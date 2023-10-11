d457d22980ebd2e18c5c861420950208aafbed7b
Alberto de Olivira <betodeoliveira@outlook.com>
2023-10-10 23:17:59 -0300

    update

--- lead-generation.js
+++ lead-generation.js
@@ -161,7 +161,7 @@ function isValidEmailEnding(email, validEndings) {
 }
 
 // Phone Checker
-let input = document.querySelector("#phone-number"),
+let input = document.querySelector("#your-phone"),
     dialCode = document.querySelector(".dial-code"),
     errorMsg = document.querySelector("#lead-gen-invalid-number"),
     validMsg = document.querySelector("#lead-gen-valid-number");
